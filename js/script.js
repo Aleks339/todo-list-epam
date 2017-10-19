9d4d76cd3cba896fa903c1dde9cddb4efb366bc7
Aleks339 <annamuljar9@gmail.com>
2017-10-19 11:07:15 +0300

    trying to set deadline

--- js/script.js
+++ js/script.js
@@ -1,148 +1,173 @@
-//array of tasks
-var taskArray = [];
+window.onload = function(){
+  //array of tasks
+  var taskArray = [];
 
-//main div.
-var todo = document.createElement('div');
-todo.setAttribute('class', 'todo');
+  //main div.
+  var todo = document.createElement('div');
+  todo.setAttribute('class', 'todo');
 
-//header
-var title = document.createElement('h1');
-title.setAttribute('class', 'todo_title');
-title.innerHTML = "Todolist";
+  //main header
+  var title = document.createElement('h1');
+  title.setAttribute('class', 'todo_title');
+  title.innerHTML = "Todolist";
 
-//time tag
-var time = document.createElement('time');
-time.setAttribute('class', 'todo_time');
+  //deadline header
+  var deadlineTitle = document.createElement('h5');
+  deadlineTitle.setAttribute('class', 'todo_deadline-title');
+  deadlineTitle.innerHTML = 'Put the deadline, please';
 
-// time input
-var timeInput = document.createElement('input');
-timeInput.setAttribute('type', 'date');
-timeInput.setAttribute('class', 'todo_time--input');
+  //time tag
+  var time = document.createElement('time');
+  time.setAttribute('class', 'todo_time');
+  time.setAttribute('datetime', '2017-10-19');
 
-//div, all tasks filter are inside of it
-var buttonTasks = document.createElement('div');
-buttonTasks.setAttribute('class', 'todo_button-task');
+  // time input
+  var timeInput = document.createElement('input');
+  timeInput.setAttribute('type', 'date');
+  timeInput.setAttribute('class', 'todo_time--input');
 
-//task filters
-var buttonTaskCompleted = document.createElement('button');
-buttonTaskCompleted.setAttribute('class', 'todo_button-task--button-task-completed');
-var buttonTaskUnCompleted = document.createElement('button');
-buttonTaskUnCompleted.setAttribute('class', 'todo_button-task--button-task-uncompleted');
-var buttonTaskAll = document.createElement('button');
-buttonTaskAll.setAttribute('class', 'todo_button-task--button-task-all');
+  //div, all tasks filter are inside of it
+  var buttonTasks = document.createElement('div');
+  buttonTasks.setAttribute('class', 'todo_button-task');
 
-//filter with completed tasks
-buttonTaskCompleted.onclick = function(){
-  for(i = 0; i < taskArray.length; i++){
-    if(taskArray[i].getAttribute('data-complete') === '1'){
+  //task filters
+  var buttonTaskCompleted = document.createElement('button');
+  buttonTaskCompleted.setAttribute('class', 'todo_button-task--button-task-completed');
+  var buttonTaskUnCompleted = document.createElement('button');
+  buttonTaskUnCompleted.setAttribute('class', 'todo_button-task--button-task-uncompleted');
+  var buttonTaskAll = document.createElement('button');
+  buttonTaskAll.setAttribute('class', 'todo_button-task--button-task-all');
+  var buttonTasksPlannedOnTomorrow = document.createElement('button');
+  buttonTasksPlannedOnTomorrow.setAttribute('class', 'todo_button-task--button-task-planned-on-tomorrow');
+  var buttonTasksPlannedOnWeek = document.createElement('button');
+  buttonTasksPlannedOnWeek.setAttribute('class', 'todo_button-task--button-task-planned-on-week');
+
+  //filter with completed tasks
+  buttonTaskCompleted.addEventListener("click", function(){
+    for(i = 0; i < taskArray.length; i++){
+      if(taskArray[i].getAttribute('data-complete') === '1'){
+        taskArray[i].style.display = 'block';
+      }else{
+        taskArray[i].style.display = 'none';
+      }
+    }
+  });
+
+  //filter with uncompleted tasks
+  buttonTaskUnCompleted.addEventListener("click", function(){
+    for(i = 0; i < taskArray.length; i++){
+      if(taskArray[i].getAttribute('data-complete') === '0'){
+        taskArray[i].style.display = 'block';
+      }else{
+        taskArray[i].style.display = 'none';
+      }
+    }
+  });
+
+  //filter with all tasks
+  buttonTaskAll.addEventListener("click", function(){
+    for(i = 0; i < taskArray.length; i++){
       taskArray[i].style.display = 'block';
-    }else{
-      taskArray[i].style.display = 'none';
     }
-  }
-}
+  });
 
-//filter with uncompleted tasks
-buttonTaskUnCompleted.onclick = function(){
-  for(i = 0; i < taskArray.length; i++){
-    if(taskArray[i].getAttribute('data-complete') === '0'){
-      taskArray[i].style.display = 'block';
-    }else{
-      taskArray[i].style.display = 'none';
-    }
-  }
-}
+  //task filter text
+  buttonTasksPlannedOnTomorrow.innerHTML = 'Tasks planned on tomorrow';
+  buttonTasksPlannedOnWeek.innerHTML = 'Tasks planned on week';
+  buttonTaskCompleted.innerHTML = 'Completed tasks';
+  buttonTaskUnCompleted.innerHTML = 'Uncompleted tasks';
+  buttonTaskAll.innerHTML = 'All tasks';
 
-//filter with all tasks
-buttonTaskAll.onclick = function(){
-  for(i = 0; i < taskArray.length; i++){
-    taskArray[i].style.display = 'block';
-  }
-}
+  //input type text to fill it by our tasks
+  var item = document.createElement('input');
+  item.setAttribute('class', 'todo_item');
+  item.setAttribute("type", "text");
+  item.setAttribute('size', '35');
 
-//task filter text
-buttonTaskCompleted.innerHTML = 'Completed tasks';
-buttonTaskUnCompleted.innerHTML = 'Uncompleted tasks';
-buttonTaskAll.innerHTML = 'All tasks';
+  //button to add tasks
+  var addItem = document.createElement('button');
+  addItem.setAttribute("type", "button");
+  addItem.innerHTML = "Add";
 
-//input type text to fill it by our tasks
-var item = document.createElement('input');
-item.setAttribute('class', 'todo_item');
-item.setAttribute("type", "text");
-item.setAttribute('size', '35');
+  //what will happen if we click on add-task button?
+  addItem.addEventListener("click", function(){
+    //div to wrap items of todolist
+    var itemWrapper = document.createElement('div');
+    itemWrapper.setAttribute('class', 'todo_item-wrapper');
+    itemWrapper.setAttribute('data-complete', '0');
 
-//button to add tasks
-var addItem = document.createElement('button');
-addItem.setAttribute("type", "button");
-addItem.innerHTML = "Add";
+    //todolist checkbox
+    var checkbox = document.createElement('input');
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("class", "todo_item-wrapper--checkbox");
 
-//what will happen if we click on add-task button?
-addItem.addEventListener("click", function(){
-  //div to wrap items of todolist
-  var itemWrapper = document.createElement('div');
-  itemWrapper.setAttribute('class', 'todo_item-wrapper');
-  itemWrapper.setAttribute('data-complete', '0');
+    //button to remove item
+    var closeItem = document.createElement('span');
+    closeItem.innerHTML = ' [X]';
+    closeItem.setAttribute('class', 'todo_item-wrapper--close');
 
-  //todolist checkbox
-  var checkbox = document.createElement('input');
-  checkbox.setAttribute("type", "checkbox");
-  checkbox.setAttribute("class", "todo_item-wrapper--checkbox");
+    //click the button to remove item
+    closeItem.addEventListener("click", function(){
+      todo.removeChild(itemWrapper);
+    });
+    //todolist text
+    var textItem = document.createElement('span');
+    //todolist text click
+    checkbox.addEventListener("click", function(){
+      if(this.checked){
+        //check the checkbox
+        textItem.style.textDecoration = 'line-through';
+        textItem.style.color = 'gray';
+        itemWrapper.setAttribute('data-complete', '1');
+      }else{
+        //uncheck the checkbox
+        textItem.style.textDecoration = 'none';
+        textItem.style.color = 'inherit';
+        itemWrapper.setAttribute('data-complete', '0');
+      }
+    });
+    //value of todo item
+    textItem.innerHTML = item.value;
 
-  //button to remove item
-  var closeItem = document.createElement('span');
-  closeItem.innerHTML = ' [X]';
-  closeItem.setAttribute('class', 'todo_item-wrapper--close');
+    //deadline span
+    var timeSpan = document.createElement('span');
+    timeSpan.innerHTML = timeInput.value;
+    var newDate = new Date();
+    function Date(){
+      this.innerHTML = ' ' + timeInput.value;
+      //filter with tasks planned on week
+      buttonTasksPlannedOnWeek.addEventListener("click", function(){
+          if (timeInput.value = newDate.getDate() + 7){
+            this.style.display = 'block';
+          }else{
+            this.style.display = 'none';
+          }
+      });
+    };
 
-  //click the button to remove item
-  closeItem.onclick = function(){
-    todo.removeChild(itemWrapper);
-  }
-  //todolist text
-  var textItem = document.createElement('span');
-  //todolist text click
-  checkbox.onclick = function() {
-    if(this.checked){
-      //check the checkbox
-      textItem.style.textDecoration = 'line-through';
-      textItem.style.color = 'gray';
-      itemWrapper.setAttribute('data-complete', '1');
-    }else{
-      //uncheck the checkbox
-      textItem.style.textDecoration = 'none';
-      textItem.style.color = 'inherit';
-      itemWrapper.setAttribute('data-complete', '0');
-    }
-  }
-  //value of todo item
-  textItem.innerHTML = item.value;
+    //push the elements into the task array
+    taskArray.push(itemWrapper);
 
-  //deadline span
-  var timeSpan = document.createElement('span');
-  timeSpan.innerHTML = timeInput.value;
-  var newDate = new Date();
-  function Date(){
-    this.innerHTML = timeInput.value;
-  }
-
-  //push the elements into the task array
-  taskArray.push(itemWrapper);
+    //append elements
+    todo.appendChild(itemWrapper);
+    itemWrapper.appendChild(checkbox);
+    itemWrapper.appendChild(textItem);
+    itemWrapper.appendChild(timeSpan);
+    itemWrapper.appendChild(closeItem);
+  });
 
   //append elements
-  todo.appendChild(itemWrapper);
-  itemWrapper.appendChild(checkbox);
-  itemWrapper.appendChild(textItem);
-  itemWrapper.appendChild(timeSpan);
-  itemWrapper.appendChild(closeItem);
-});
-
-//append elements
-document.body.appendChild(todo);
-todo.appendChild(title);
-todo.appendChild(time);
-time.appendChild(timeInput);
-todo.appendChild(buttonTasks);
-buttonTasks.appendChild(buttonTaskCompleted);
-buttonTasks.appendChild(buttonTaskUnCompleted);
-buttonTasks.appendChild(buttonTaskAll);
-todo.appendChild(item);
-todo.appendChild(addItem);
+  document.body.appendChild(todo);
+  todo.appendChild(title);
+  todo.appendChild(deadlineTitle);
+  todo.appendChild(time);
+  time.appendChild(timeInput);
+  todo.appendChild(buttonTasks);
+  buttonTasks.appendChild(buttonTasksPlannedOnTomorrow);
+  buttonTasks.appendChild(buttonTasksPlannedOnWeek);
+  buttonTasks.appendChild(buttonTaskCompleted);
+  buttonTasks.appendChild(buttonTaskUnCompleted);
+  buttonTasks.appendChild(buttonTaskAll);
+  todo.appendChild(item);
+  todo.appendChild(addItem);
+}
